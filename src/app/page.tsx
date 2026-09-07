1c5c539516748396ae9b059cd14eeeeabcdc8ac9
Mike-Benn <mbenn49@wgu.edu>
2026-09-07 04:03:16 -0400

    Placeholder home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,14 @@ import { PageWrapper } from '@/components/PageWrapper';
 import { auth } from '@/auth/auth';
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
+import { LogoutButton } from '@/components/Buttons/LogoutButton';
 
 export default async function HomePage() {
   const session = await auth.api.getSession({ headers: await headers() });
   if (!session) redirect('/login');
   return (
-    <PageWrapper>
-      <span>This is my home page</span>
+    <PageWrapper wrapperClassName="p-6">
+      <LogoutButton />
     </PageWrapper>
   );
 }
