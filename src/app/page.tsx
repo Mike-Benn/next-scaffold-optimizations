8acb235ffaff56d9bfe8b763dacc5968fdf7e3c0
Mike-Benn <mbenn49@wgu.edu>
2026-09-20 02:49:25 -0400

    Add verification toast to home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,12 +3,14 @@ import { auth } from '@/auth/auth';
 import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { LogoutButton } from '@/components/Buttons/LogoutButton';
+import { VerificationToast } from '@/app/_components/VerificationToast';
 
 export default async function HomePage() {
   const session = await auth.api.getSession({ headers: await headers() });
   if (!session) redirect('/auth/login');
   return (
     <PageWrapper wrapperClassName="p-6">
+      <VerificationToast />
       <LogoutButton />
     </PageWrapper>
   );
