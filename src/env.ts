192d194166927f47b0ad78d923864612bdb9b41c
Mike-Benn <mbenn49@wgu.edu>
2026-09-20 02:43:54 -0400

    Add NODE_ENV

--- src/env.ts
+++ src/env.ts
@@ -1,6 +1,7 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
+  NODE_ENV: z.enum(['development', 'production', 'test']),
   DATABASE_URL: z.string(),
   BETTER_AUTH_SECRET: z.string(),
   BETTER_AUTH_URL: z.string(),
