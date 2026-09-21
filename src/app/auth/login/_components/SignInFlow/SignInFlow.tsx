10d3221129c1a9a0a6a1cde6958beda133a9d0c3
Mike-Benn <mbenn49@wgu.edu>
2026-09-20 23:14:26 -0400

    Fix race condition bug

--- src/app/auth/login/_components/SignInFlow/SignInFlow.tsx
+++ src/app/auth/login/_components/SignInFlow/SignInFlow.tsx
@@ -10,10 +10,11 @@ type View = 'form' | 'verification';
 export function SignInFlow() {
   const [view, setView] = useState<View>('form');
   const [email, setEmail] = useState('');
-  const { data: session } = authClient.useSession();
+  const { data: session, isPending } = authClient.useSession();
 
   // Redirects automatically if sitting on verify email view and verification is successful in a different tab
   useEffect(() => {
+    if (isPending) return;
     if (session?.user?.emailVerified) {
       window.location.href = '/';
     }
