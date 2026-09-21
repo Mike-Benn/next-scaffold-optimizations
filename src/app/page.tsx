5c312935227436f19ec576bff609263c3eb2f9b8
Mike-Benn <mbenn49@wgu.edu>
2026-09-20 23:23:38 -0400

    Implement SessionObserver

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,12 +4,14 @@ import { headers } from 'next/headers';
 import { redirect } from 'next/navigation';
 import { LogoutButton } from '@/components/Buttons/LogoutButton';
 import { VerificationToast } from '@/app/_components/VerificationToast';
+import { SessionObserver } from '@/components/SessionObserver';
 
 export default async function HomePage() {
   const session = await auth.api.getSession({ headers: await headers() });
   if (!session) redirect('/auth/login');
   return (
     <PageWrapper wrapperClassName="p-6">
+      <SessionObserver />
       <VerificationToast />
       <LogoutButton />
     </PageWrapper>
